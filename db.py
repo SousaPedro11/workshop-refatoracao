46a9ae539d62bebd92e4973c5c2f1881335cb9fd
Pedro Sousa <ppls2106@gmail.com>
2020-08-05 16:38:33 -0300

    add gerenciamento conexao e logging

--- db.py
+++ db.py
@@ -1,7 +1,35 @@
+import logging
 import os
+
 import psycopg2
+import psycopg2.extras
+from flask import g
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+
+def conexao_gerenciada():
+    if 'conexao' not in g:
+        g.conexao = abrir_conexao()
+    return g.conexao
+
+
+def liberar_conexao_gerenciada(testando=False):
+    conexao = g.pop('conexao', None)
+    if conexao is not None:
+        if not testando:
+            conexao.commit()
+        else:
+            conexao.rollback()
+        conexao.close()
 
 
 def abrir_conexao():
-  # Exemplo: DB_CONN_STRING="dbname=banco1 user=postgres password=postgres host=localhost"
-  return psycopg2.connect(os.environ['DB_CONN_STRING'])
+    # Exemplo: DB_CONN_STRING="dbname=banco1 user=postgres password=postgres host=localhost"
+    conexao = psycopg2.connect(
+        os.environ['DB_CONN_STRING'],
+        connection_factory=psycopg2.extras.LoggingConnection
+    )
+    conexao.initialize(logger)
+    return conexao
